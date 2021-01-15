9b87a0417bf75796fd4f989c7652a3a28da7df58
17etro <petrpetryk2016@gmail.com>
2021-01-15 08:48:10 +0200

    fix problems with push

--- static/js/13.74ce33c6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp24-rols"]=this["webpackJsonp24-rols"]||[]).push([[13],{259:function(s,t,o){"use strict";o.r(t);var r=o(0),e=(o(1),o(88));t.default=function(s){return Object(r.jsx)(e.a,{forMobile:!0})}}}]);
-//# sourceMappingURL=13.74ce33c6.chunk.js.map
