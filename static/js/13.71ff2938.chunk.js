99054c4440e385cc062767dc5dec1d955b67f125
17etro <petrpetryk2016@gmail.com>
2021-01-28 17:20:24 +0200

    111

--- static/js/13.71ff2938.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp24-rols"]=this["webpackJsonp24-rols"]||[]).push([[13],{261:function(s,t,o){"use strict";o.r(t);var r=o(0),e=(o(1),o(88));t.default=function(s){return Object(r.jsx)(e.a,{forMobile:!0})}}}]);
-//# sourceMappingURL=13.71ff2938.chunk.js.map
---
+++ static/js/13.f5227007.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp24-rols"]=this["webpackJsonp24-rols"]||[]).push([[13],{259:function(s,t,o){"use strict";o.r(t);var r=o(0),e=(o(1),o(86));t.default=function(s){return Object(r.jsx)(e.a,{forMobile:!0})}}}]);
+//# sourceMappingURL=13.f5227007.chunk.js.map
